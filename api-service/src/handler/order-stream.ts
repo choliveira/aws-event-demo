818a5a3c8c654cff5fbfef95b5014d4383e111b6
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-10-07 00:55:53 +1000

    fix

--- api-service/src/handler/order-stream.ts
+++ api-service/src/handler/order-stream.ts
@@ -3,7 +3,7 @@ import { unmarshall } from '@aws-sdk/util-dynamodb';
 export const handler = async (event: any): Promise<any> => {
   try {
     console.log('order-stream handler', JSON.stringify(event));
-    const order = unmarshall(event.Records[0].newImage);
+    const order = unmarshall(event.Records[0].dynamodb.NewImage);
     console.log('order unmarshalled', JSON.stringify(order));
   } catch (error: any) {
     console.log('Error found in the order-stream handler', JSON.parse(error));
