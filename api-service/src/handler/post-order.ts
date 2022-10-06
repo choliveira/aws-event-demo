1502bb3428745455bdb2cf8955fbc328662e49d1
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-10-06 17:16:48 +1000

    Initial setup for order endpoint and database

--- api-service/src/handler/post-order.ts
+++ api-service/src/handler/post-order.ts
@@ -1,18 +1,15 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-import { paymentController } from '../controller/payment-controller';
 
 export const handler = async (
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2> => {
   try {
-    const body = JSON.parse(event.body!);
-    const payment = await paymentController(body);
     return {
       statusCode: 200,
-      body: JSON.stringify(payment)
+      body: JSON.stringify({ data: 'OK' })
     };
   } catch (error: any) {
-    console.log('Error found in the post-payment handler', JSON.parse(error));
+    console.log('Error found in the post-order handler', JSON.parse(error));
     return {
       statusCode: 500,
       body: JSON.stringify(error)
