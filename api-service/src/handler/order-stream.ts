0aa7564282921096178c9e21674df21cc0125888
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-10-07 14:33:53 +1000

    Looping through the records and acting when and order is inserted.

--- api-service/src/handler/order-stream.ts
+++ api-service/src/handler/order-stream.ts
@@ -3,8 +3,30 @@ import { unmarshall } from '@aws-sdk/util-dynamodb';
 export const handler = async (event: any): Promise<any> => {
   try {
     console.log('order-stream handler', JSON.stringify(event));
-    const order = unmarshall(event.Records[0].dynamodb.NewImage);
-    console.log('order unmarshalled', JSON.stringify(order));
+
+    const records = event.Records;
+    records.forEach((stream: any) => {
+      if (stream.dynamodb.eventName !== 'INSERT') {
+        return;
+      }
+
+      const order = unmarshall(stream.dynamodb.NewImage);
+      console.log('order unmarshalled', JSON.stringify(order));
+      /** TODO:
+       * 1 - Produce a message to SQS
+       * 2 - Publish a message to an SNS Topic
+       * 3 - Send a message to an Event Bus (EventBridge)
+       * */
+
+      //1 - Produce a message to SQS
+      sqsProducer(order);
+
+      // 2 - Publish a message to an SNS Topic
+      snsPublisher(order);
+
+      // 3 - Send a message to an Event Bus (EventBridge)
+      eventBusPublisher(order);
+    });
   } catch (error: any) {
     console.log('Error found in the order-stream handler', JSON.parse(error));
     return {
@@ -12,4 +34,25 @@ export const handler = async (event: any): Promise<any> => {
       body: JSON.stringify(error)
     };
   }
+};
+
+const sqsProducer = (data: any) => {
+  console.log(
+    'Im the sqsProducer and I will send this data as a message payload to SQS.',
+    data
+  );
+};
+
+const snsPublisher = (data: any) => {
+  console.log(
+    'Im the snsPublisher and I will send this data as a message payload to an SNS Topic.',
+    data
+  );
+};
+
+const eventBusPublisher = (data: any) => {
+  console.log(
+    'Im the eventBusPublisher and I will send this data as a message payload to an SNS Topic.',
+    data
+  );
 };
