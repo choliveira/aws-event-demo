b4e96c90703559afc1707f248522128448187efd
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-10-08 22:21:50 +1000

    Added redriveDLQ tp sqs-service

--- api-service/src/aws-services/sqs-service.ts
+++ api-service/src/aws-services/sqs-service.ts
@@ -1,6 +1,7 @@
 import {
   SendMessageCommand,
   SendMessageCommandOutput,
+  SetQueueAttributesCommand,
   SQSClient
 } from '@aws-sdk/client-sqs';
 
@@ -27,7 +28,7 @@ export class SqsService {
   ): Promise<SendMessageCommandOutput> {
     const { source, title, payload, queueUrl } = data;
     const params = {
-      DelaySeconds: 60,
+      DelaySeconds: 10,
       MessageAttributes: {
         Title: {
           DataType: 'String',
@@ -48,6 +49,7 @@ export class SqsService {
       QueueUrl: queueUrl
     };
     try {
+      await this.redriveDLQ();
       console.log('Will send message to sqs...', params);
       const data = await this.sqsClient.send(new SendMessageCommand(params));
       if (!data) {
@@ -56,12 +58,34 @@ export class SqsService {
       console.log('Success, message sent. MessageID:', data.MessageId);
       return data;
     } catch (err: any) {
-      console.error(
+      console.log(
         'Failed to send message to the queue',
         err,
         'check payload',
         data
       );
+      throw new Error(err);
+    }
+  }
+
+  private async redriveDLQ(): Promise<void> {
+    const params = {
+      Attributes: {
+        RedrivePolicy:
+          '{"deadLetterTargetArn":"arn:aws:sqs:ap-southeast-2:587919987702:dlq-process-order-created",' +
+          '"maxReceiveCount":"10"}' //DEAD_LETTER_QUEUE_ARN
+      },
+      QueueUrl:
+        'https://sqs.ap-southeast-2.amazonaws.com/587919987702/dlq-process-order-created' //SQS_QUEUE_URL
+    };
+    try {
+      console.log('Will set attributes to redriveDLQ');
+      const data = await this.sqsClient.send(
+        new SetQueueAttributesCommand(params)
+      );
+      console.log('redriveDLQ Success', data);
+    } catch (err: any) {
+      console.log('Error in the method redriveDLQ', err);
       throw new Error(err);
     }
   }
