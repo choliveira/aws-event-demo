6442e7576cf5ac45b214328f194638316d7e7120
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-10-09 00:38:13 +1000

    more tests

--- api-service/src/handler/order-stream.ts
+++ api-service/src/handler/order-stream.ts
@@ -1,36 +1,38 @@
 import { SendMessageCommand } from '@aws-sdk/client-sqs';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { DynamoDBStreamEvent } from 'aws-lambda';
-import { OrderStreamController } from '../controller/order-stream-controller';
 import { sqsClient } from '../utils/sqs';
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<any> => {
   try {
-    const orderStreamController = new OrderStreamController();
+    // const orderStreamController = new OrderStreamController();
     console.log('order-stream handler', JSON.stringify(event));
-    await run();
-    console.log('Response from sending message to sqs in th handler');
+    // await run();
+    // console.log('Response from sending message to sqs in th handler');
     const records = event.Records;
-    // records.forEach(async (stream: any) => {
-    //   if (stream.eventName !== 'INSERT') {
-    //     return;
-    //   }
+    records.forEach(async (stream: any) => {
+      if (stream.eventName !== 'INSERT') {
+        return;
+      }
 
-    //   const order = unmarshall(stream.dynamodb.NewImage);
-    //   console.log('order unmarshalled', JSON.stringify(order));
+      const order = unmarshall(stream.dynamodb.NewImage);
+      console.log('order unmarshalled', JSON.stringify(order));
 
-    //   //1 - Produce a message to SQS
-    //   const controller = await orderStreamController.sqsProducer(order);
-    //   console.log(
-    //     'Response from sending message to sqs in th handler',
-    //     controller
-    //   );
+      //1 - Produce a message to SQS
+      // const controller = await orderStreamController.sqsProducer(order);
+      // console.log(
+      //   'Response from sending message to sqs in th handler',
+      //   controller
+      // );
+      await run(JSON.stringify(order));
+      console.log('Response from sending message to sqs in th handler');
 
-    //   // 2 - Publish a message to an SNS Topic
-    //   // orderStreamController.snsPublisher(order);
+      // 2 - Publish a message to an SNS Topic
+      // orderStreamController.snsPublisher(order);
 
-    //   // 3 - Send a message to an Event Bus (EventBridge)
-    //   // orderStreamController.eventBusPublisher(order);
-    // });
+      // 3 - Send a message to an Event Bus (EventBridge)
+      // orderStreamController.eventBusPublisher(order);
+    });
   } catch (error: any) {
     console.log('Error found in the order-stream handler', JSON.parse(error));
     return {
@@ -40,31 +42,29 @@ export const handler = async (event: DynamoDBStreamEvent): Promise<any> => {
   }
 };
 
-const params = {
-  DelaySeconds: 10,
-  MessageAttributes: {
-    Title: {
-      DataType: 'String',
-      StringValue: 'The Whistler'
+const run = async (data: string) => {
+  const params = {
+    DelaySeconds: 10,
+    MessageAttributes: {
+      Title: {
+        DataType: 'String',
+        StringValue: 'The Whistler'
+      },
+      Author: {
+        DataType: 'String',
+        StringValue: 'John Grisham'
+      },
+      WeeksOn: {
+        DataType: 'Number',
+        StringValue: '6'
+      }
     },
-    Author: {
-      DataType: 'String',
-      StringValue: 'John Grisham'
-    },
-    WeeksOn: {
-      DataType: 'Number',
-      StringValue: '6'
-    }
-  },
-  MessageBody:
-    'Information about current NY Times fiction bestseller for week of 12/11/2016.',
-  // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-  // MessageGroupId: "Group1",  // Required for FIFO queues
-  QueueUrl:
-    'https://sqs.ap-southeast-2.amazonaws.com/587919987702/process-order-created' //SQS_QUEUE_URL; e.g., 'https://sqs.REGION.amazonaws.com/ACCOUNT-ID/QUEUE-NAME'
-};
-
-const run = async () => {
+    MessageBody: data,
+    // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+    // MessageGroupId: "Group1",  // Required for FIFO queues
+    QueueUrl:
+      'https://sqs.ap-southeast-2.amazonaws.com/587919987702/process-order-created' //SQS_QUEUE_URL; e.g., 'https://sqs.REGION.amazonaws.com/ACCOUNT-ID/QUEUE-NAME'
+  };
   try {
     console.log('Will send message to SQS...', params);
     const data = await sqsClient.send(new SendMessageCommand(params));
