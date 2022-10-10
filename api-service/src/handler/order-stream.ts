d90e2492a4e4865a6549fddad230c0a3bdee27d6
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-10-10 17:30:07 +1000

    sending message outside looping with static payload

--- api-service/src/handler/order-stream.ts
+++ api-service/src/handler/order-stream.ts
@@ -6,35 +6,35 @@ export const handler = async (event: DynamoDBStreamEvent): Promise<any> => {
   try {
     // const orderStreamController = new OrderStreamController();
     console.log('order-stream handler', JSON.stringify(event));
-    // await run();
-    // console.log('Response from sending message to sqs in th handler');
-    const records = event.Records;
-    records.forEach(async (stream: any) => {
-      if (stream.eventName !== 'INSERT') {
-        return;
-      }
+    await run('');
+    console.log('Response from sending message to sqs in th handler');
+    // const records = event.Records;
+    // records.forEach(async (stream: any) => {
+    //   if (stream.eventName !== 'INSERT') {
+    //     return;
+    //   }
 
-      // const order = DynamoDB.Converter.unmarshall(stream.dynamodb.NewImage);
-      const order = stream.dynamodb.Keys;
-      console.log('order unmarshalled', JSON.stringify(order));
+    //   // const order = DynamoDB.Converter.unmarshall(stream.dynamodb.NewImage);
+    //   // const order = stream.dynamodb.Keys;
+    //   // console.log('order unmarshalled', JSON.stringify(order));
 
-      //1 - Produce a message to SQS
-      // const controller = await orderStreamController.sqsProducer(order);
-      // console.log(
-      //   'Response from sending message to sqs in th handler',
-      //   controller
-      // );
+    //   //1 - Produce a message to SQS
+    //   // const controller = await orderStreamController.sqsProducer(order);
+    //   // console.log(
+    //   //   'Response from sending message to sqs in th handler',
+    //   //   controller
+    //   // );
 
-      //TODO: continue from here... the issue is not with the unmarshall
-      await run(JSON.stringify(order));
-      console.log('Response from sending message to sqs in th handler');
+    //   //TODO: continue from here... the issue is not with the unmarshall
+    //   // await run(JSON.stringify(order));
+    //   // console.log('Response from sending message to sqs in th handler');
 
-      // 2 - Publish a message to an SNS Topic
-      // orderStreamController.snsPublisher(order);
+    //   // 2 - Publish a message to an SNS Topic
+    //   // orderStreamController.snsPublisher(order);
 
-      // 3 - Send a message to an Event Bus (EventBridge)
-      // orderStreamController.eventBusPublisher(order);
-    });
+    //   // 3 - Send a message to an Event Bus (EventBridge)
+    //   // orderStreamController.eventBusPublisher(order);
+    // });
   } catch (error: any) {
     console.log('Error found in the order-stream handler', JSON.parse(error));
     return {
