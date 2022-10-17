a71b3fcd2462e6b967c1a53d39484399a3b23fc7
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-10-17 21:40:36 +1000

    Implemented ses

---
+++ api-service/src/aws-services/ses-service.ts
@@ -0,0 +1,68 @@
+import { SendEmailCommand, SESClient } from '@aws-sdk/client-ses';
+
+export interface IEmailData {
+  to: [string];
+  subject: string;
+  body: string;
+}
+
+export class SesService {
+  private region: string = 'ap-southeast-2';
+  private sesClient: SESClient;
+  private senderEmailAddress: string;
+
+  constructor(sender: string) {
+    this.senderEmailAddress = sender;
+    this.sesClient = new SESClient({
+      credentials: {
+        accessKeyId: process.env.ACCESS_KEY!,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY!
+      },
+      region: this.region
+    });
+  }
+
+  public async sendEmail(emailData: IEmailData) {
+    const emailCommand = this.SetEmailCommand(emailData);
+    try {
+      return await this.sesClient.send(emailCommand);
+    } catch (e) {
+      console.error('Failed to send email.', e, 'Email data:', emailCommand);
+      return e;
+    }
+  }
+
+  private SetEmailCommand(emailData: IEmailData): SendEmailCommand {
+    return new SendEmailCommand({
+      Destination: {
+        /* required */
+        CcAddresses: [
+          /* more items */
+        ],
+        ToAddresses: emailData.to
+      },
+      Message: {
+        /* required */
+        Body: {
+          /* required */
+          Html: {
+            Charset: 'UTF-8',
+            Data: emailData.body
+          },
+          Text: {
+            Charset: 'UTF-8',
+            Data: emailData.body
+          }
+        },
+        Subject: {
+          Charset: 'UTF-8',
+          Data: emailData.subject
+        }
+      },
+      Source: this.senderEmailAddress,
+      ReplyToAddresses: [
+        /* more items */
+      ]
+    });
+  }
+}
--- api-service/src/handler/delivery-service.ts
+++ api-service/src/handler/delivery-service.ts
@@ -1,14 +1,19 @@
-export const handler = async (event: any): Promise<any> => {
+// import { SQSEvent } from 'aws-lambda';
+import { deliveryController } from '../controller/delivery-controller';
+
+export const handler = async (event: any): Promise<void> => {
   try {
+    //TODO: REMOVE THIS AFTER TESTING LOCALLY
+    const orders = JSON.parse(event.body!);
+    // const orders = event.Records;
     console.log(
       'delivery-service handler received message from sqs',
-      JSON.stringify(event)
+      JSON.stringify(orders)
     );
-  } catch (error: any) {
-    console.log('Error found in the order-stream handler', JSON.parse(error));
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error)
-    };
+    await deliveryController(orders);
+    console.log('Email dispatched, just check recipient inbox');
+  } catch (e: any) {
+    console.error('Error found in the delivery-service handler', JSON.parse(e));
+    throw new Error(e);
   }
 };
