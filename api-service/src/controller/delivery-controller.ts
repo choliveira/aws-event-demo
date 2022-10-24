4b4c0c0d0caa94d34fec0d93ee9790d33e6f0ba2
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-10-24 19:24:16 +1000

    hack to send one single email.

--- api-service/src/controller/delivery-controller.ts
+++ api-service/src/controller/delivery-controller.ts
@@ -9,30 +9,39 @@ export const deliveryController = async (
 ): Promise<void> => {
   try {
     const emailService = new SesService(sender);
-
-    return records.forEach(async (r: SQSRecord) => {
-      const order = JSON.parse(r.body);
-      console.log(
-        'delivery-controller extracted order from event records',
-        order
-      );
-
-      const tracker: string = retrieveAusPostTracker();
-
-      const emailData: IEmailData = {
-        to: [order.customer.email],
-        subject: `We are working on your delivery for order: ${order.id}`,
-        body: `We received the request for ${order.delivery.type} delivery to your address at: ${order.delivery.address} for your order: ${order.id}. Please use this number ${tracker} to track your order on AusPost`
-      };
-
-      console.log('delivery-controller just set email data as: ', emailData);
-
-      await emailService.sendEmail(emailData);
-
-      console.log(
-        'delivery-controller email should have been sent via ses-service without issues'
-      );
+    let orders: any[] = records.map((r: SQSRecord) => {
+      return (orders.push = JSON.parse(r.body));
     });
+
+    if (orders.length <= 0) {
+      return;
+    }
+
+    /**
+     * TODO: This is a hack for the demo to just get the first order coming from the event and send one email,
+     * otherwise we need to refactor ses-service to send bulk email
+     * */
+    const order = orders[0];
+
+    console.log(
+      'delivery-controller extracted order from event records',
+      order
+    );
+    const tracker: string = retrieveAusPostTracker();
+
+    const emailData: IEmailData = {
+      to: [order.customer.email],
+      subject: `We are working on your delivery for order: ${order.id}`,
+      body: `We received the request for ${order.delivery.type} delivery to your address at: ${order.delivery.address} for your order: ${order.id}. Please use this number ${tracker} to track your order on AusPost`
+    };
+
+    console.log('delivery-controller just set email data as: ', emailData);
+
+    await emailService.sendEmail(emailData);
+
+    console.log(
+      'delivery-controller email should have been sent via ses-service without issues'
+    );
   } catch (e) {
     console.log('Failed on delivery-controller', JSON.stringify(e));
   }
