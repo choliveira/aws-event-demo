111914ca0a4bb913d119f77129a013ee553e7c04
Carlos H. Oliveira <contact@carlosholiveira.com>
2022-11-03 19:42:09 +1000

    Initial setup for event-tracker-table

---
+++ api-service/src/handler/event-tracker-stream.ts
@@ -0,0 +1,20 @@
+import { DynamoDBStreamEvent } from 'aws-lambda';
+
+export const handler = async (event: DynamoDBStreamEvent): Promise<any> => {
+  try {
+    console.log('Starting event-tracker-stream handler', event);
+    /** Use this when run locally */
+    //@ts-ignore
+    // const body = JSON.parse(event.body!);
+    // const records = body.Records;
+  } catch (error: any) {
+    console.log(
+      'Error found in the event-tracker-stream handler',
+      JSON.parse(error)
+    );
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    };
+  }
+};
